c2324f51db4705aefe8ea0a9973fa2a68c95f835
Adrian Faustino <contact.adrianfausitno@gmail.com>
2020-08-07 10:24:59 -0400

    wrote outline and constructor

---
+++ src/Email.java
@@ -0,0 +1,28 @@
+// tutorial reference: https://www.youtube.com/watch?v=U3Ibvu0htNs
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // constructor to receive first name and last name
+    public Email (String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // ask for department
+
+    // generate random password
+
+    // set mailbox capacity
+
+    // set alternate email
+
+    // change password
+
+    // get
+}
